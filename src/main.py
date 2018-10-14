6ae3593141864eead3de1663466b367e324fd059
marko64humans <marko.64humans@gmail.com>
2018-10-14 21:00:34 +0200

    Bot can now connect to the server

--- src/main.py
+++ src/main.py
@@ -2,5 +2,21 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
+from discord.ext.commands import Bot
 import asyncio
 import time
+
+"""Some setup"""
+Clinet = discord.Client()
+client = commands.Bot(command_prefix="!") #this command_prefix is not used the command prefix should be defined in the variable below
+
+"""Constants"""
+bot_command_prefix = "!" #change this prefix to you liking
+token = "" #enter your token here
+
+@client.event
+async def on_ready(): #executes when the bot is ready to process messages
+    print("Bot is ready!")
+
+
+client.run(token)
